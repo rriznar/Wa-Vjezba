a2c66cc1ebb1de9306b0c36dc6813b50d7f81485
rriznar <rikardo141099@gmail.com>
2021-03-26 13:18:54 +0100

    Dodavanje materijala

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import express from 'express';
+
+const app = express()  // instanciranje aplikacije
+const port = 3000  // port na kojem će web server slušati
+
+app.get('/', (req, res) => res.send('Hello World'))
+
+app.listen(port, () => console.log(`Slušam na portu ${port}!`))
